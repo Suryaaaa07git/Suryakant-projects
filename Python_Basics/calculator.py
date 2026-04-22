051bc28cd40f8a916ad06bc0a69888644ff6818b
Suryaaaa07git <photos.suryakant@gmail.com>
2026-04-23 00:16:52 +0530

    Create calculator.py

---
+++ Python_Basics/calculator.py
@@ -0,0 +1,18 @@
+# Simple Calculator
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print("Addition:", add(10, 5))
+print("Subtraction:", sub(10, 5))
+print("Multiplication:", mul(10, 5))
+print("Division:", div(10, 5))
